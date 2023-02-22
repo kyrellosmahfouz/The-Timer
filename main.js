7f3158b22a147be80d57cc73d5223ce9bcf8ce3c
kyrellosmahfouz <125634261+kyrellosmahfouz@users.noreply.github.com>
2023-02-22 22:50:57 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -1,18 +1,53 @@
+// Start Header Menu
+var menu_icon = document.querySelector("#menu_icon")
+var header_menu = document.querySelector("#header_menu")
+function display_menu() {
+    menu_icon.onclick = function () {
+        header_menu.style.display = "block"
+        menu_icon.onclick = function () {
+            header_menu.style.display = "none"
+            display_menu()
+        }
+    }
+}
+display_menu()
+// End Header Menu
+
+
 var checkbox_for_birthday = document.getElementById("cbfbir")
 var birthday_section = document.getElementById("birthday")
 checkbox_for_birthday.onclick = birthday_display
 function birthday_display() {
     if (checkbox_for_birthday.checked == true) {
-        birthday_section.style.display = "block";
+        birthday_section.style.display = "flex";
+        window.localStorage.birthday_display = true
     } else {
         birthday_section.style.display = "none";
+        window.localStorage.birthday_display = false
     }
 }
 
+window.onload = _ => {
+    if (window.localStorage.birthday_display == "false") {
+        birthday_section.style.display = "none"
+        checkbox_for_birthday == false
+    }
+}
 
+// Start Reset Birthday
+// var input_birthday = document.getElementById("input_Birthday")
+// var Reset_Birthday_Button = document.getElementById("Reset-Birthday-Button")
+// Reset_Birthday_Button.addEventListener("click", Reset_Birthday)
+// function Reset_Birthday() {
+//     birsec = new Date(`${input_birthday.value} 00:00:00`).getTime()
+//     console.log(birsec)
+//     return birsec
+// }
+// birsec = Reset_Birthday
+// End Reset Birthday
 
 function birthday() {
-    birsec = new Date("APR 8,2023 00:00:00").getTime()
+    var birsec = new Date("Apr 8 , 2023 00:00:00").getTime()
 
     datenow = new Date().getTime()
 
